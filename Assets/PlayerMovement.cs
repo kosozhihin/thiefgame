ff0e587d0a4e9f52bea7bec5e477dc4df41e6c18
Aleksei Kosozhihin <a.kosozhihin@gmail.com>
2017-06-03 13:45:28 +0200

    Added simple player controller

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 moveDirection = Vector3.zero;
+    public float speed = 6.0F;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    CharacterController controller = GetComponent<CharacterController>();
+	    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	    moveDirection = transform.TransformDirection(moveDirection);
+	    moveDirection *= speed;
+	    controller.Move(moveDirection * Time.deltaTime);
+    }
+}
