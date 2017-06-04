17a263697b2c758075d97333ce3af055b881f292
Aleksei Kosozhihin <a.kosozhihin@gmail.com>
2017-06-04 07:49:29 +0200

    Added player controls dependance on camera orientation

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -9,6 +9,8 @@ public class CameraMovement : MonoBehaviour
     private float desiredRot;
     public float rotSpeed = 0.0000025F;
 
+    public CameraOrientationEnum CameraOrientation = CameraOrientationEnum.North;
+
     void Start()
     {
 
@@ -21,26 +23,51 @@ public class CameraMovement : MonoBehaviour
                                 //makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
         if (Math.Abs(this.desiredRot) < 0.0000000001)
         {
-            if (Input.GetKey(KeyCode.Q))
-                this.desiredRot = 90;
             if (Input.GetKey(KeyCode.E))
+                this.desiredRot = 90;
+            if (Input.GetKey(KeyCode.Q))
                 this.desiredRot = -90;
 
         }
 
         if (Math.Abs(this.desiredRot) > 0.0000000001)
         {
-            print(Time.deltaTime);
             var rotationDelta = Time.deltaTime * this.rotSpeed;
             if (rotationDelta > Math.Abs(this.desiredRot))
                 rotationDelta = Math.Abs(this.desiredRot);
-            if (this.desiredRot < 0)
+            bool rotateClockwise = this.desiredRot > 0;
+            if (!rotateClockwise)
                 rotationDelta *= -1;
             this.desiredRot -= rotationDelta;
-            print(string.Format("Rotation left: {0}, rotation now: {1}", this.desiredRot, rotationDelta));
-
+            if (Math.Abs(this.desiredRot) < 0.0000000001)
+            {
+                var orientationValues = (CameraOrientationEnum[]) Enum.GetValues(typeof(CameraOrientationEnum));
+                var currentIndex = Array.FindIndex(orientationValues, value => value == this.CameraOrientation);
+                if (rotateClockwise)
+                {
+                    currentIndex--;
+                    if (currentIndex < 0)
+                        currentIndex = orientationValues.Length - 1;
+                }
+                else
+                {
+                    currentIndex++;
+                    if (currentIndex > orientationValues.Length - 1)
+                        currentIndex = 0;
+                }
+                this.CameraOrientation = orientationValues[currentIndex];
+                Debug.Log(this.CameraOrientation);
+            }
+            
             this.transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), rotationDelta);
         }
+    }
 
+    public enum CameraOrientationEnum
+    {
+        North,
+        West,
+        South,
+        East
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Vector3 moveDirection = Vector3.zero;
     public float speed = 6.0F;
+
+    public CameraMovement Camera;
 
     // Use this for initialization
     void Start () {
@@ -14,10 +15,26 @@ public class PlayerMovement : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	    CharacterController controller = GetComponent<CharacterController>();
-	    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-	    moveDirection = transform.TransformDirection(moveDirection);
-	    moveDirection *= speed;
-	    controller.Move(moveDirection * Time.deltaTime);
+	    CharacterController controller = this.GetComponent<CharacterController>();
+	    switch (this.Camera.CameraOrientation)
+	    {
+	        case CameraMovement.CameraOrientationEnum.North:
+	            this.moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	            break;
+            case CameraMovement.CameraOrientationEnum.West:
+	            this.moveDirection = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+	            break;
+	        case CameraMovement.CameraOrientationEnum.South:
+	            this.moveDirection = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+	            break;
+	        case CameraMovement.CameraOrientationEnum.East:
+	            this.moveDirection = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+	            break;
+	    }
+	    this.moveDirection = this.transform.TransformDirection(this.moveDirection);
+	    this.moveDirection *= this.speed;
+	    if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+	        this.moveDirection *= 2F;
+	    controller.Move(this.moveDirection * Time.deltaTime);
     }
 }
